57892258a677cfa9f8357097297751b4743fbf00
wanglq <535346881@qq.com>
2022-06-26 14:03:09 +0800

    完成

--- main.js
+++ main.js
@@ -1,3 +1,41 @@
 import './style.less'
+import contentInfo from './src/data/content'
 
+const ACTIVE = 'active'
 
+const videos = document.querySelectorAll('video')
+const toggle = document.querySelector('.menu-toggle')
+const content = document.querySelector('.text-description')
+const main = document.querySelector('.mian-structure')
+const buttons = document.querySelectorAll('.aside-menu a')
+const overlay = document.querySelector('.video-overlay')
+overlay.className = 'video-overlay video-overlay-island'
+toggle.addEventListener('click', () => {
+  toggle.classList.toggle(ACTIVE)
+  main.classList.toggle(ACTIVE)
+})
+buttons.forEach((ele, index) => {
+  ele.addEventListener('click', (e) => {
+    setContent(index)
+    setVideoActive(index)
+    
+  })
+})
+const setVideoActive = (i) => {
+  videos.forEach((ele, index) => {
+    ele.classList.remove(ACTIVE)
+    if (index === i) {
+      ele.classList.add(ACTIVE)
+    }
+  })
+}
+
+const setContent = (i) => {
+  overlay.className =  `video-overlay video-overlay-${contentInfo[i].name}`
+  content.children[0].innerHTML = contentInfo[i].h1
+  content.children[1].innerHTML = contentInfo[i].h2
+  content.children[2].innerHTML = contentInfo[i].p
+}
+
+setVideoActive(0)
+setContent(0)
---
+++ src/data/content.js
@@ -0,0 +1,28 @@
+export default [{
+    h1: 'Never Stop',
+    h2: 'Explore The World',
+    p: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, quaerat! Porro quidem cupiditate optio necessitatibus ipsam dolores aliquid, sit a modi ratione consequuntur non atque. Reprehenderit perferendis explicabo excepturi iste?',
+    name: 'island'
+  },
+  {
+    h1: 'Spring',
+    h2: 'Feel the new vitality',
+    p: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum debitis molestiae itaque iure praesentium suscipit ducimus similique repellendus dolores eligendi, adipisci consectetur nulla sed minus impedit, quo tenetur vero consequatur.',
+    name: 'spring'
+  }, {
+    h1: 'Summer',
+    h2: 'Enjoy passionate time',
+    p: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore numquam maiores iste. Consectetur aut dolor officia magnam illum consequatur est perferendis dolorem, doloremque aspernatur harum numquam asperiores eligendi vitae consequuntur?',
+    name: 'summer',
+  }, {
+    h1: 'Autumn',
+    h2: 'Experience without limits',
+    p: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia nisi impedit ad facere! Molestias nemo deserunt velit eos perferendis sit magnam dicta neque aperiam. Quibusdam aliquam praesentium modi pariatur enim.',
+    name: 'autumn'
+  }, {
+    h1: 'Winter',
+    h2: 'Enjoy peace and freedom',
+    p: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi vero nisi quibusdam corporis consequatur, tempore omnis ipsum. Minus ex non modi, saepe eius iste voluptatum temporibus molestiae rerum, possimus beatae.',
+    name: 'winter'
+  },
+]
